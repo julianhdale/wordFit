b58b012fcb8015fa96c7eeb5fb5602f2b223a7d0
Julian Dale <jdale@solstice-mobile.com>
2015-06-19 12:23:13 -0500

    removed unneeded json property

--- wordfit/app/models/SaveTextPayload.java
+++ wordfit/app/models/SaveTextPayload.java
@@ -6,10 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
  * Created by juliandale on 6/18/15.
  */
 public class SaveTextPayload {
-
-    @JsonProperty("userId")
-    public String userId;
-
+    
     @JsonProperty("terms")
     public String[] terms;
 
